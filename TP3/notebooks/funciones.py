229c7b39274770cad709b578f821b75628094bb5
marcelosaied <marcelosaied@gmail.com>
2022-06-12 21:04:21 -0300

    TP# - init

---
+++ TP3/notebooks/funciones.py
@@ -0,0 +1,62 @@
+###   General ###########################################################################################
+#todo natriz de confusions
+def print_matriz_confusion():
+    plt.rcParams['figure.figsize'] = (8, 6)
+    plt.rcParams['font.size'] = 16
+    sns.heatmap(data.iloc[:, :4].corr(), vmin = -1, vmax = 1, center = 0, cmap = "YlGnBu", annot = True)
+    
+def get_info(dflocal):
+    print(dflocal.head(3))
+    print(dflocal.shape)
+
+    
+    
+###   regrsion lineal simple ###################################################################################
+def get_rls(X,y,columna):
+    # Como estamos trabajando con observaciones ordenadas en el tiempo, ponemos
+    # shuffle=False para evitar data leakage    shuffle=False
+    Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, random_state = 1, shuffle = False)
+    lm = LinearRegression() # Fiteamos el modelo sobre los vectores X e y.
+    model = lm.fit(Xtrain, ytrain)
+    print(f'''    Coef\t{model.coef_}''')
+    print(f'''    intercept\t{model.intercept_}''')
+    test_sklearn = np.array(Xtest).reshape(-1,1)
+    ypred = model.predict(test_sklearn)
+    
+    print(f'''    MAE\t{mean_absolute_error(ytest, ypred).round(2)}
+    MSE\t{mean_squared_error(ytest, ypred).round(2)}
+    RMSE\t{np.sqrt(mean_squared_error(ytest, ypred)).round(2)}
+    R2\t{r2_score(ytest, ypred).round(2)}
+    ''')
+
+    sns.regplot(data = data, x = columna, y = 'precio_por_m2', ci = 95, scatter_kws = {"color": "blue", 's': 10}, line_kws = {"color": "red"})
+    #return mean_absolute_error(y_test, y_pred)
+
+ 
+###   regrsion lineal multiple ###################################################################################
+def rlm(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=False)
+    linreg = LinearRegression()
+    linreg.fit(X_train, y_train)
+    y_pred = linreg.predict(X_test)
+        
+    n = len(y_train)
+    p = X_train.shape[1]
+    R2 = r2_score(y_test, y_pred)
+    R2_ajustado = 1 - (1 - R2)*((n-1)/(n - p - 1))
+    
+    print(f'''    
+    Intercepto\t{linreg.intercept_.round(4)}
+    MAE\t\t{mean_absolute_error(y_test, y_pred).round(4)}
+    MSE\t\t{mean_squared_error(y_test, y_pred).round(4)}
+    RMSE\t{np.sqrt(mean_squared_error(y_test, y_pred)).round(4)}
+    R2\t\t{(R2).round(4)}
+    R2ADJ\t{(R2_ajustado).round(4)}
+    ''')
+
+    plt.plot(y,y, '-.',c='grey')
+    plt.scatter(y_pred, y_test, s=30, c='r', marker='+', zorder=10)
+    plt.xlabel("Valores predichos")
+    plt.ylabel("Valores reales")
+    plt.title('Predicción de precio por m$^2$ utilizando RLM')
+    plt.show()
